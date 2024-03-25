e619fe8edecf0625ba95222f83d77e57fae30313
Matheus Frauches <frauches.27@gmail.com>
2024-03-25 16:02:39 -0300

    Implements PostsService to get all blog posts and parse its data to json

---
+++ _data/posts/primeiro-post.md
@@ -0,0 +1,14 @@
+---
+date: 2024-03-25
+title: "My new job"
+slug: /new-job
+url: https://www.linkedin.com/in/matheus-frauches
+excerpt: "It's a new stage in my life"
+tags:
+  - new-job
+  - company-X
+---
+
+# How it started!
+
+Ipsum laboris velit officia esse culpa labore veniam id magna cillum cupidatat Lorem. Ut quis ex tempor consectetur laboris ex nisi proident. Sit ex ullamco excepteur amet exercitation adipisicing adipisicing et consequat.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import PostsService from "@src/services/posts/PostsService";
 import { withTemplateConfig } from "@src/services/template/withTemplateConfig";
 export { default } from "@src/screens/HomeScreen/HomeScreen";
 
 export async function getStaticProps() {
-  return { props: await withTemplateConfig({ exemplo: "Teste" }) };
+  const posts = await PostsService().getAll();
+
+  console.log("paaaaa", posts.length);
+
+  return { props: await withTemplateConfig({ posts, exemplo: "Teste" }) };
 }
